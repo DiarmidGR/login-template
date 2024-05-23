23e6f66502c719557c1831bd886a54ff850e7f5a
Diarmid Rendell <diarmid@hotmail.ca>
2024-05-23 13:53:39 -0600

    refactored routes code

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,3 +1,5 @@
+const dotenv = require('dotenv');
+dotenv.config();
 module.exports = {
     jwtSecret: process.env.JWT_SECRET,
     port: process.env.PORT || 3001
--- backend/server.js
+++ backend/server.js
@@ -1,41 +1,17 @@
 const express = require('express');
-const bcrypt = require('bcryptjs');
 const cors = require('cors');
-const jwt = require('jsonwebtoken');
-const dotenv = require('dotenv');
+const authController = require('./controllers/auth.controller');
 const app = express();
-
-
-dotenv.config();
-const db = require('./config/db.config');
+const authRoutes = require('./routes/auth.route');
+const config = require('./config/config');
 
 app.use(express.json(), cors());
 
-app.listen(3001, ()=> {
-    console.log('Server running on port 3001');
+app.use('/api/auth', authRoutes);
+
+app.listen(config.port, ()=> {
+    console.log('Server running on port ' + config.port);
 });
 
 // Login Route
-app.post('/login', (req, res) => {
-    const {username, password} = req.body;
-    if (!username || !password) {
-        return res.status(400).json({message: 'Please provide both username and password.'});
-    }
-
-    const query = 'SELECT * FROM users WHERE username = ?';
-    db.query(query, [username], async (err, results) => {
-        if (err) throw err;
-        if (results.length === 0) {
-            return res.status(401).json({message: 'User not found.'});
-        }
-
-        const user = results[0];
-        const isMatch = await bcrypt.compare(password, user.password);
-        if (!isMatch){
-            return res.status(401).json({message: 'Invalid credentials.'});
-        }
-
-        const token = jwt.sign({id: user.id}, process.env.JWT_SECRET, {expiresIn: '1h'});
-        res.json({token});
-    });
-});
+app.post('/login', authController.login);
