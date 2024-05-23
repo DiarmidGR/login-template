08c666eef7dfafdb9d870e45d40ec31b9161941c
Diarmid Rendell <diarmid@hotmail.ca>
2024-05-23 15:03:51 -0600

    fixed jwt token authorization

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const cors = require('cors');
-const authController = require('./controllers/auth.controller');
 const app = express();
 const authRoutes = require('./routes/auth.route');
 const config = require('./config/config');
+
 
 app.use(express.json(), cors());
 
@@ -12,6 +12,3 @@ app.use('/api/auth', authRoutes);
 app.listen(config.port, ()=> {
     console.log('Server running on port ' + config.port);
 });
-
-// Login Route
-app.post('/login', authController.login);
